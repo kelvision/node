9db591716551c85353cee38f851c38d0004cb224
Kelvision Development <kelvisiondevelopment@admins-MacBook-Pro.local>
2021-01-22 20:29:48 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the homepage baby");
+});
+
+
+app.listen(3000, () => {
+
+    console.log("Wazzzaaapppp");
+});
+
