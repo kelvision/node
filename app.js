97dc745e47ca5a91cd3f4d4612e67f3e004a77a8
Kelvision Development <kelvisiondevelopment@admins-MacBook-Pro.local>
2021-01-22 20:59:36 -0500

    Variable Port

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.get('/', (req, res) => {
     res.send("Welcome to the homepage baby");
 });
 
-
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
 
     console.log("Wazzzaaapppp");
 });
